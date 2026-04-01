c821443ec784b17735e7275e7d3c3a35cb6bcbef
Alexandre Oliveira <alexandreroliveira76@gmail.com>
2026-04-01 20:46:45 -0300

    feat: adiciona models para times de futebol

---
+++ src/br/com/futebolbet/models/Partida.java
@@ -0,0 +1,28 @@
+package br.com.futebolbet.models;
+
+import java.time.LocalDateTime;
+
+public class Partida {
+    private Clube clubeCasa;
+    private Clube clubeFora;
+    private Resultado resultado;
+    private LocalDateTime dataHora;
+
+    public Partida(Clube clubeCasa, Clube clubeFora, LocalDateTime dataHora){
+        this.clubeCasa = clubeCasa;
+        this.clubeFora = clubeFora;
+        this.dataHora = dataHora;
+    }
+
+    public Resultado getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Resultado resultado) {
+        this.resultado = resultado;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+}
